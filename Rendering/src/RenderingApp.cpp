f24430e9c8761d86d574c408f0ac3593bc4d62ca
MaxGuidry <mguidry7@outlook.com>
2017-09-01 17:48:53 -0500

    circle and cube

--- Rendering/src/RenderingApp.cpp
+++ Rendering/src/RenderingApp.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include "tiny_obj_loader.h"
 
+Mesh circle = Mesh();
 
 glm::mat4 grid =
 glm::mat4(
@@ -13,6 +14,12 @@ glm::mat4(
 	0, 1, 0, 0,
 	0, 0, 1, 0,
 	-5, 0, -5, 1);
+glm::mat4 circleT =
+glm::mat4(
+	1, 0, 0, 0,
+	0, 1, 0, 0,
+	0, 0, 1, 0,
+	0, -1, 0, 1);
 RenderingApp::RenderingApp() : camera(new DollyCamera())
 {
 }
@@ -41,19 +48,19 @@ Mesh * RenderingApp::GenGrid(unsigned int rows, unsigned int cols)
 	for (unsigned int r = 0; r < (rows - 1); ++r)
 		for (unsigned int c = 0; c < (cols - 1); ++c)
 		{
-			auiIndices[index++] = r * cols + c;			
-			auiIndices[index++] = (r + 1) * cols + c;			
+			auiIndices[index++] = r * cols + c;
+			auiIndices[index++] = (r + 1) * cols + c;
 			auiIndices[index++] = (r + 1) * cols + (c + 1);
-			
+
 			auiIndices[index++] = r * cols + c;
 			auiIndices[index++] = (r + 1) * cols + (c + 1);
 			auiIndices[index++] = r * cols + (c + 1);
 		}
 
-	
-	
-	
-	
+
+
+
+
 	for (unsigned int i = 0; i < (rows * cols); i++)
 		verts.push_back(aoVerts[i]);
 	for (unsigned int i = 0; i < index; i++)
@@ -108,7 +115,7 @@ Mesh box = Mesh();
 
 bool RenderingApp::Start()
 {
-	
+
 	camera->LookAt(glm::vec3(15, 15, 15), glm::vec3(5, 0, 5), glm::vec3(0, 1, 0));
 	const char * vsSource = FileRead("./vertex.vert");
 	const char * fsSource = FileRead("./fragment.frag");
@@ -144,47 +151,80 @@ bool RenderingApp::Start()
 	Vertex b = { glm::vec4(1,0,0,1),glm::vec4(1,1,1,1) };
 	Vertex c = { glm::vec4(0,1,0,1),glm::vec4(1,1,1,1) };
 	Vertex d = { glm::vec4(1,1,0,1),glm::vec4(1,1,1,1) };
-	Vertex e = { glm::vec4(0,2,0,1),glm::vec4(1,1,1,1) };
-	Vertex f = { glm::vec4(1,2,0,1),glm::vec4(1,1,1,1) };
-	Vertex g = { glm::vec4(0,3,0,1),glm::vec4(1,1,1,1) };
+	Vertex e = { glm::vec4(0,1,1,1),glm::vec4(1,1,1,1) };
+	Vertex f = { glm::vec4(1,1,1,1),glm::vec4(1,1,1,1) };
+	Vertex g = { glm::vec4(0,0,1,1),glm::vec4(1,1,1,1) };
+	Vertex h = { glm::vec4(1,0,1,1),glm::vec4(1,1,1,1) };
 
-	Vertex h = { glm::vec4(1,3,0,1),glm::vec4(1,1,1,1) };
-	Vertex i = { glm::vec4(0,4,0,1),glm::vec4(1,1,1,1) };
-	Vertex j = { glm::vec4(1,4,0,1),glm::vec4(1,1,1,1) };
-	Vertex k = { glm::vec4(0,5,0,1),glm::vec4(1,1,1,1) };
-	Vertex l = { glm::vec4(1,5,0,1),glm::vec4(1,1,1,1) };
-	Vertex m = { glm::vec4(0,6,0,1),glm::vec4(1,1,1,1) };
-	Vertex n = { glm::vec4(1,6,0,1),glm::vec4(1,1,1,1) };
-	std::vector<Vertex> boxVerts{ a,b,c,d,e,f,g,h,i,j,k,l,m,n };
+
+	std::vector<Vertex> boxVerts{ a,b,c,d,e,f,g,h };
 	std::vector<unsigned int> boxindeces
 	{
-		/*0,1,3,
-		3,0,2,
-		2,4,0,
-		0,4,6,
-		6,7,5,
-		5,6,4,
-		4,2,3,
-		3,4,5,
-		5,3,1,
-		1,5,7,
-		7,6,1,
-		1,6,0*/
 		0,2,1,
 		1,2,3,
-		3,5,2,
-		2,5,4,
+
+		2,4,3,
+		3,4,5,
+
 		4,6,5,
 		5,6,7,
-		7,9,6,
-		6,9,8,
-		8,10,9,
-		9,10,11,
-		11,13,10,
-		10,12,13
+
+		1,3,7,
+		7,3,5,
+
+		6,0,7,
+		7,0,1,
+
+		6,4,0,
+		0,4,2
 
 	};
-	
+
+#pragma region Circle
+	std::vector<Vertex> circleVerts = std::vector<Vertex>();
+	std::vector<unsigned int> circleIndices = std::vector<unsigned>();
+	unsigned int i = 0;
+	for (float x = -2.f; x < 2.f; x += .08f)
+	{
+		circleVerts.push_back(Vertex{ glm::vec4(x, 0, glm::sqrt(4.f - x*x), 1),glm::vec4(1,1,1,1) });
+		circleIndices.push_back(i);
+		i++;
+
+		circleIndices.push_back(i);
+	}
+	circleIndices.push_back(i);
+	for (float x = 2.f; x > -2.f; x -= .08f)
+	{
+		circleVerts.push_back(Vertex{ glm::vec4(x, 0, -glm::sqrt(4.f - x*x), 1),glm::vec4(1,1,1,1) });
+		if (i + 1 < 100)
+			circleIndices.push_back(i + 1);
+		i++;
+
+		if (i < 100)
+			circleIndices.push_back(i);
+
+	}
+
+	circleIndices.push_back(0);
+	/*circleVerts.push_back(Vertex{ glm::vec4(0,2,0,0),glm::vec4(1,1,1,1) });
+	circleVerts.push_back(Vertex{ glm::vec4(1.4,1.4,0,0),glm::vec4(1,1,1,1) });
+	circleVerts.push_back(Vertex{ glm::vec4(2,0,0,0),glm::vec4(1,1,1,1) });
+	circleVerts.push_back(Vertex{ glm::vec4(1.4,-1.4,0,0),glm::vec4(1,1,1,1) });
+	circleVerts.push_back(Vertex{ glm::vec4(0,-2,0,0),glm::vec4(1,1,1,1) });
+	circleVerts.push_back(Vertex{ glm::vec4(-1.4,-1.4,0,0),glm::vec4(1,1,1,1) });
+	circleVerts.push_back(Vertex{ glm::vec4(-2,0,0,0),glm::vec4(1,1,1,1) });
+	circleVerts.push_back(Vertex{ glm::vec4(-1.4,1.4,0,0),glm::vec4(1,1,1,1) });
+
+	for (unsigned int i = 0; i < 8; i++)
+	{
+		circleIndices.push_back(i);
+		circleIndices.push_back(i);
+	}*/
+	circle.initialize(circleVerts, circleIndices);
+	circle.create_buffers();
+
+#pragma endregion 
+
 	box.initialize(boxVerts, boxindeces);
 	box.create_buffers();
 	box.bind();
@@ -197,7 +237,7 @@ static double pmouseY = 0;
 bool RenderingApp::Update(float deltaTime)
 {
 	static bool mouse_down = false;
-	
+
 	if (mouse_down == false)
 	{
 		mouse_down = true;
@@ -255,14 +295,31 @@ bool RenderingApp::Draw()
 	glDrawElements(GL_TRIANGLES, mesh->index_Count, GL_UNSIGNED_INT, 0);
 	mesh->unbind();
 	box.bind();
-	/*grid = grid * glm::mat4(
+	grid = grid * glm::mat4(
 		glm::cos(.01f), 0, -glm::sin(.01f), 0,
 		0, 1, 0, 0,
 		glm::sin(.01f), 0, glm::cos(.01f), 0,
-		0, 0, 0, 1);*/
+		0, 0, 0, 1);
 	glUniformMatrix4fv(projectionViewUniform, 1, false, glm::value_ptr(camera->getProjectionView()*  grid));
 	glDrawElements(GL_TRIANGLES, box.index_Count, GL_UNSIGNED_INT, 0);
-	glUseProgram(0);
 	box.unbind();
+	circle.bind();
+	glm::mat4 x  = glm::mat4(
+		glm::cos(.05f), glm::sin(.05f), 0, 0,
+		-glm::sin(.05f), glm::cos(.05f), 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	);
+	glm::mat4 y  = glm::mat4(
+		1,0,0,0,
+		0, glm::cos(.05f), glm::sin(.05f), 0,
+		0,-glm::sin(.05f), glm::cos(.05f), 0,
+		0,0,0,1
+	);
+	circleT = circleT * x * y;
+	glUniformMatrix4fv(projectionViewUniform, 1, false, glm::value_ptr(camera->getProjectionView() * circleT ));
+	glDrawElements(GL_LINES, circle.index_Count, GL_UNSIGNED_INT, 0);
+	circle.unbind();
+	glUseProgram(0);
 	return true;
 }
